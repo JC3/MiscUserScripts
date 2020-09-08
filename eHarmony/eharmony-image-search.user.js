eee7733182687704ffd266b833e5b227a4933d77
Jason C <JC3@users.noreply.github.com>
2020-09-08 12:54:26 -0400

    Create eharmony-image-search.user.js

---
+++ eHarmony/eharmony-image-search.user.js
@@ -0,0 +1,94 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jason C
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// ==UserScript==
+// @name         eHarmony Google Image Search
+// @namespace    jasonc
+// @updateURL    https://raw.githubusercontent.com/JC3/MiscUserScripts/master/eHarmony/eharmony-image-search.user.js
+// @downloadURL  https://raw.githubusercontent.com/JC3/MiscUserScripts/master/eHarmony/eharmony-image-search.user.js
+// @version      1
+// @description  Adds a reverse image search button to match images.
+// @author       Jason Cipriani
+// @match        *://*.eharmony.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const gisButtonStyle = `
+        .jc-gis-button {
+            position: absolute;
+            left: 3px;
+            bottom: 6px;
+            display: inline-block;
+            border-radius: 4px;
+            font: var(--FONT_50_highlight);
+            text-transform: uppercase;
+            background-color: hsla(var(--COLOR_alertInfo_100-h), var(--COLOR_alertInfo_100-s), var(--COLOR_alertInfo_100-l), 80%);
+            padding: 4px 6px 4px 6px;
+            margin: 0 0 0 4px;
+            color: var(--COLOR_support2_100);
+            z-index: 1000000;
+        }
+        .jc-gis-button:hover {
+            filter: brightness(150%);
+        }
+    `;
+
+    var $ = window.jQuery;
+
+    $('<style/>')
+        .attr('type', 'text/css')
+        .text(gisButtonStyle)
+        .appendTo(document.head);
+
+    new MutationObserver(function (ms) {
+        updatePhotoLinks();
+    }).observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    function updatePhotoLinks () {
+        $(':not(div.photoMask, .js-openSlideshow) > .u-photoProtector + .photo[style]:not([data-jc-gis-fixed=1])')
+            .attr('data-jc-gis-fixed', 1)
+            .append($('<div/>')
+                .addClass('jc-gis-button')
+                .text('GIS')
+                .click(execImageSearch));
+    }
+
+    function execImageSearch (e) {
+        let url = $(e.target).parent().css('background-image')
+            .replace(/^url\(["']?/, '')
+            .replace(/["']?\)$/, '')
+            .trim();
+        if (url != "") {
+            window.open(`https://www.google.com/searchbyimage?image_url=${encodeURIComponent(url)}`, 'jc-gis-window');
+        }
+        return false;
+    }
+
+})();
