e307adc27d042246572241193acaccb7111ae014
Jason C <JC3@users.noreply.github.com>
2020-09-24 20:47:22 -0400

    Create eharmony-linkify.js

---
+++ eHarmony/eharmony-linkify.js
@@ -0,0 +1,57 @@
+/*
+MIT License
+
+Copyright (c) 2020 Jason C
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+// ==UserScript==
+// @name         eHarmony Message Links
+// @namespace    jasonc
+// @updateURL    https://raw.githubusercontent.com/JC3/MiscUserScripts/master/eHarmony/eharmony-linkify.js
+// @downloadURL  https://raw.githubusercontent.com/JC3/MiscUserScripts/master/eHarmony/eharmony-linkify.js
+// @version      1
+// @description  Turns URLs in eHarmony messages into clickable links.
+// @author       Jason Cipriani
+// @match        *://*.eharmony.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var $ = window.jQuery;
+
+    new MutationObserver(function () {
+        $('#conversationArea .messageText:not([data-jc-linkified="1"])')
+            .attr('data-jc-linkified', 1)
+            .find('.body')
+            .each((_,e) => linkify(e))
+    }).observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    function linkify (e) {
+        var html = e.innerHTML.replace(/(https?|ftp):\/\/[^ <]*/, '<a href="$&" target="_blank">$&</a>');
+        e.innerHTML = html;
+    }
+
+})();
